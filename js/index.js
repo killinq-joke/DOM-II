347c08e90e4c459ceba2aa7fb11b219f697d1edb
Zak <zakadmaster@gmail.com>
2020-01-14 14:55:29 +0100

    Addition of mouseover, keydown & focus

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', event => {
+    console.log('mouseovered');
+    nav.style.backgroundColor = 'red' ;
+})
+
+
+const body = document.querySelector('body');
+let i = 0;
+document.addEventListener('keydown', event => {
+    console.log('keydowned');
+    if(Math.random() > 0.5 && i === 0) {
+        body.style.backgroundColor = 'turquoise';
+    } else {
+        body.style.backgroundColor = 'pink';
+    }
+    i++
+})
+
+const password = document.querySelector('input[type="password"]');
+console.log(password);
+password.addEventListener('focus', event => {
+    console.log('focused');
+    nav.style.backgroundColor = 'blue' ;
+})
